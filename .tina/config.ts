624ba76861b2313936a83b48320c84062c994e98
Henk Jan Wils <henkjan@paypro.nl>
2022-11-25 15:07:16 +0100

    Update config.ts

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,11 +1,15 @@
+import {  defineConfig } from 'tinacms'
+
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD
+
 export default defineConfig({
-  //...
-  token: process.env.NEXT_PUBLIC_TINA_TOKEN // generated on app.tina.io,
+  token: process.env.NEXT_PUBLIC_TINA_TOKEN, // generated on app.tina.io,
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // generated on app.tina.io
   branch,
   schema: {
-    collections: [
-    //...
-    // See https://tina.io/docs/schema/ for more info about "collections"
-  ]}
+    // ...
+  },
 })
